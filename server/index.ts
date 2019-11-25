b73c350af97d62e74e09765339a8bb055e8320f8
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-11-25 18:52:43 +0300

    Add express server support

---
+++ server/index.ts
@@ -0,0 +1,39 @@
+// tslint:disable no-console
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+
+const PORT = 3000;
+const HOST = process.env.HOST || 'localhost';
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = createServer((req, res) => {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url as any, true);
+
+      handle(req, res, parsedUrl);
+    });
+    server.on('error', (err: any) => {
+      if (err.code === 'EADDRINUSE') {
+        console.log('Address in use, retrying...');
+        setTimeout(() => {
+          server.close();
+          server.listen(PORT, HOST);
+        }, 1000);
+      }
+      console.log('ERROR: %s', err.message);
+    });
+    server.listen(PORT, () => {
+      console.log('> Ready on http://localhost:3000');
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
