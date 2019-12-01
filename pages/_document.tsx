92df2a57f8e96065ab4b4c7c489f026d2f4901e5
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-12-01 13:50:45 +0300

    Integrate DI website as assets source

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
+const ASSETS_SOURCE_URL = process.env.ASSETS_SOURCE_URL || 'http://159.65.56.142/';
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -10,10 +12,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link href={ `${ASSETS_SOURCE_URL}assets/css/screen.min.css` } rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700" rel="stylesheet"/>
+          <noscript><link href={ `${ASSETS_SOURCE_URL}assets/icons/icon.fallback.css` } rel="stylesheet"/></noscript>
+          <script src={ `${ASSETS_SOURCE_URL}assets/js/libs/modernizr.js` }/>
+          <link rel="icon" type="image/png" href={ `${ASSETS_SOURCE_URL}assets/favicons/favicon.png` } sizes="16x16"/>
+        </Head>
         <body>
           <Main />
           <NextScript />
+          <script src={ `${ASSETS_SOURCE_URL}assets/js/app.bundle.js` }/>
         </body>
       </Html>
     );
--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
 // tslint:disable no-console
+import { config } from 'dotenv';
+config();
 import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
