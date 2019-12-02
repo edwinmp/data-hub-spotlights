deb49c4130ea3ad4a6a621bd8da564fddaf1a024
Edwin P. Magezi <edwinm_p@yahoo.com>
2019-12-03 00:52:57 +0300

    Add basic header to default layout

---
+++ components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header role="banner" className="header">
+      <div className="row">
+        <a href="/" className="branding">Development Initiatives</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
