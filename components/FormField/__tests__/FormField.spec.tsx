bd80c64f6153720c57254d8f08fa16d4fd09a749
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-01-27 18:53:52 +0300

    Add FormField

---
+++ components/FormField/__tests__/FormField.spec.tsx
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import * as TestRenderer from 'react-test-renderer';
+import { FormField } from '../FormField';
+
+describe('FormField', () => {
+  test('renders correctly', () => {
+    const renderer = TestRenderer.create(
+      <FormField/>
+    ).toJSON();
+
+    expect(renderer).toMatchSnapshot();
+  });
+
+  test('renders with children correctly', () => {
+    const renderer = TestRenderer.create(
+      <FormField><div>My Child</div></FormField>
+    ).toJSON();
+
+    expect(renderer).toMatchSnapshot();
+  });
+});
