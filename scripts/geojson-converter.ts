6c8508d2722fe20610686057990241d2b04bd1aa
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-01-30 17:04:33 +0300

    Add geojson converter for Uganda

---
+++ scripts/geojson-converter.ts
@@ -0,0 +1,16 @@
+import * as readline from 'readline';
+import { convertUG } from './utils/geojson';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter the country code e.g UG, KE e.t.c? ', (answer) => {
+  const sourcePath = './scripts/source/';
+  if (answer === 'UG') {
+    convertUG(sourcePath);
+  }
+
+  rl.close();
+});
