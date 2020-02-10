454ee0964568875f2db392e68a31de3787f9b188
mariamiah <mariah.kakande@gmail.com>
2020-02-10 21:56:32 +0300

    modify ci file

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "install dependencies"
+yarn install
+
 echo "Clearing old build files..."
 rm -rf build.zip build
 
@@ -12,7 +15,21 @@ mkdir build && mv .next build/ && mv dist/* build/ && mv package.json build/ && 
 
 zip -q -r build.zip build
 
-echo "Bundling complete... returning repo to original state"
+echo "Bundling complete..."
+
+GITHUB_TOKEN=$1
+# Ensure that the GITHUB_TOKEN secret is included
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+#echo "Release to github"
+
+# Upload the artifact
+#curl -X POST -H "Content-Type:application/octet-stream" --data-binary @build.zip https://uploads.github.com/repos/devinit/data-hub-spotlights/releases/v0.1.0/assets?name=build.zip
+
+echo "returning repo to original state"
 
 git reset .
 git checkout .
