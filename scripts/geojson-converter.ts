8c87577759b5c7c72e3087dc2dd57c92963af2f0
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-02-17 21:21:11 +0300

    Add script folder to linter

--- scripts/geojson-converter.ts
+++ scripts/geojson-converter.ts
@@ -6,7 +6,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Enter the country code e.g UG, KE e.t.c? ', (answer) => {
+rl.question('Enter the country code e.g UG, KE e.t.c? ', answer => {
   const sourcePath = './scripts/source/';
   if (answer === 'UG') {
     convertUG(sourcePath);
--- scripts/utils/geojson.ts
+++ scripts/utils/geojson.ts
@@ -1,31 +1,31 @@
 import * as fs from 'fs';
 
 const convertUgandaDistrictGeoJSON = (features: any[]): any[] =>
-features.map(({ geometry, type, properties }) => ({
-  type,
-  properties: {
-    geocode: `UG.d${properties.DCODE2014}`,
-    name: properties.DNAME2014,
-    region: properties.Region
-  },
-  geometry
-}));
+  features.map(({ geometry, type, properties }) => ({
+    type,
+    properties: {
+      geocode: `UG.d${properties.DCODE2014}`,
+      name: properties.DNAME2014,
+      region: properties.Region
+    },
+    geometry
+  }));
 
 const getUgandaDistrictCodeByName = (districtName: string, features: any[]): number => {
-const district = features.find(feature => feature.properties.DNAME2014.toLowerCase() === districtName.toLowerCase());
+  const district = features.find(feature => feature.properties.DNAME2014.toLowerCase() === districtName.toLowerCase());
 
-return district ? district.properties.DCODE2014 : districtName;
+  return district ? district.properties.DCODE2014 : districtName;
 };
 
 const convertUgandaSubCountyGeoJSON = (features: any[], districtFeatures: any[]): any[] =>
-features.map(({ geometry, type, properties }) => ({
-  type,
-  geometry,
-  properties: {
-    geocode: `UG.d${getUgandaDistrictCodeByName(properties.DName2016, districtFeatures)}.${properties.scode}`,
-    name: properties.SName2016
-  }
-}));
+  features.map(({ geometry, type, properties }) => ({
+    type,
+    geometry,
+    properties: {
+      geocode: `UG.d${getUgandaDistrictCodeByName(properties.DName2016, districtFeatures)}.${properties.scode}`,
+      name: properties.SName2016
+    }
+  }));
 
 export const convertUG = (sourcePath: string) => {
   const districtJSON = JSON.parse(fs.readFileSync(`${sourcePath}UG/district.json`, 'utf8'));
@@ -37,7 +37,7 @@ export const convertUG = (sourcePath: string) => {
       ...convertUgandaSubCountyGeoJSON(subcountyGeoJSON.features, districtJSON.features)
     ]
   };
-  fs.writeFile('UG.json', JSON.stringify(convertedJSON), 'utf8', (err) => {
+  fs.writeFile('UG.json', JSON.stringify(convertedJSON), 'utf8', err => {
     if (err) {
       throw err;
     }
