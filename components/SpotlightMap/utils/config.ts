206e8b741d69af1534fa560c3554f014d1e51379
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-02-19 11:48:25 +0300

    Add Kenya settings to config

--- components/SpotlightMap/utils/config.ts
+++ components/SpotlightMap/utils/config.ts
@@ -18,15 +18,30 @@ export const config: { [key: string]: MapConfig } = {
   UG: {
     layers: [
       {
-        type: 'shapefile', // can be shapefile or geojson - controls how the layer is handled
-        style: 'mapbox://styles/edwinmp/ck6an0ra90nob1ikvysfmbg15/draft', // mapbox style url
-        source: 'uganda_districts_2019_i-9qg3nj', // mapbox source-layer ID - found under Select data -> Source
+        type: 'shapefile',
+        style: 'mapbox://styles/edwinmp/ck6an0ra90nob1ikvysfmbg15/draft',
+        source: 'uganda_districts_2019_i-9qg3nj',
         center: [32.655221, 1.344666],
         zoom: 6.1,
         minZoom: 6,
         maxZoom: 7,
-        nameProperty: 'DName2019', // the data property on the layer that corresponds to the location's name
-        codeProperty: 'dc2018' // the data property on the layer that corresponds to the location's code
+        nameProperty: 'DName2019',
+        codeProperty: 'dc2018'
+      }
+    ]
+  },
+  KE: {
+    layers: [
+      {
+        type: 'shapefile',
+        style: 'mapbox://styles/edwinmp/ck6rgk8bs5sfd1it4z0jjqtd7/draft',
+        source: 'ken_admbnda_adm1_iebc_2018060-1lmh46',
+        center: [37.703, 0.482],
+        zoom: 5.6,
+        minZoom: 5.4,
+        maxZoom: 6,
+        nameProperty: 'ADM1_EN',
+        codeProperty: 'ADM1_PCODE'
       }
     ]
   }
