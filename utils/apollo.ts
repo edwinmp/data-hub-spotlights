b4f9f6898bb8fbd416e23b2016ddf3e29b692ff7
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-02-21 10:12:10 +0300

    Add node-fetch to apollo as backup

--- utils/apollo.ts
+++ utils/apollo.ts
@@ -1,10 +1,12 @@
-import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import fetch from 'node-fetch';
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://212.111.41.68:8000/graphql'
+const link = createHttpLink({
+  uri: 'http://212.111.41.68:8000/graphql',
+  fetch: window.fetch || fetch
 });
 
 export const graphClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
