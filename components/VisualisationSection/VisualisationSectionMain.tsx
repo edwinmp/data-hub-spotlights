16ae66d0e0e457bc10d3ee863cb6b9fa55f98e6a
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-02-27 18:21:24 +0300

    Add width prop to VisualisationSectionMain

--- components/VisualisationSection/VisualisationSectionMain.tsx
+++ components/VisualisationSection/VisualisationSectionMain.tsx
@@ -1,12 +1,13 @@
 import React, { FunctionComponent } from 'react';
 
-const VisualisationSectionMain: FunctionComponent = ({ children }) => (
+const VisualisationSectionMain: FunctionComponent<{ width?: string }> = ({ children, width }) => (
   <div className="spotlight__main">
     {children}
     <style jsx>{`
       .spotlight__main :global(.leaflet-container) {
         background: inherit;
       }
+      ${width ? 'width: ' + width : ''}
     `}</style>
   </div>
 );
