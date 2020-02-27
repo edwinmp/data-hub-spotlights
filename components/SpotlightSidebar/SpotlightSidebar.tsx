f9e392b2f1d61a2cbe47ad991343e9acf0601677
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-02-27 16:56:41 +0300

    Add SpotlightInteractive to Sidebar children

--- components/SpotlightSidebar/SpotlightSidebar.tsx
+++ components/SpotlightSidebar/SpotlightSidebar.tsx
@@ -1,13 +1,17 @@
-import React, { Children, FunctionComponent, isValidElement } from 'react';
 import classNames from 'classnames';
-import { SidebarContent } from './SidebarContent';
+import React, { Children, FunctionComponent, isValidElement } from 'react';
 import { SpotlightHeading } from '../SpotlightHeading';
+import { SpotlightInteractive } from '../SpotlightInteractive';
+import { SidebarContent } from './SidebarContent';
 
 const SpotlightSidebar: FunctionComponent<{ className?: string }> = ({ children, className }) => {
   return (
     <div className={classNames('spotlight__aside', className)}>
       {Children.map(children, child =>
-        isValidElement(child) && (child.type === SidebarContent || child.type === SpotlightHeading) ? child : null
+        isValidElement(child) &&
+        (child.type === SidebarContent || child.type === SpotlightHeading || child.type === SpotlightInteractive)
+          ? child
+          : null
       )}
       <style jsx>{`
         min-height: 500px;
