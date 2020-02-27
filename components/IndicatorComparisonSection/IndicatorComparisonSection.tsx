a2a4c6a408fabcf5a0390e020d509f460d280329
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-02-27 15:01:26 +0300

    Add SpotlightFilters to comparison section

--- components/IndicatorComparisonSection/IndicatorComparisonSection.tsx
+++ components/IndicatorComparisonSection/IndicatorComparisonSection.tsx
@@ -1,6 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { PageSection, PageSectionHeading } from '../PageSection';
-import { SpotlightLocation, SpotlightTheme } from '../../utils';
+import { SpotlightLocation, SpotlightTheme, SpotlightOptions, SpotlightIndicator } from '../../utils';
+import { SpotlightComparison } from '../SpotlightComparison';
+import { SpotlightFilters } from '../SpotlightFilters';
+import { SpotlightBanner } from '../SpotlightBanner';
 
 export interface IndicatorComparisonSectionProps {
   location?: SpotlightLocation;
@@ -10,11 +13,52 @@ export interface IndicatorComparisonSectionProps {
 }
 
 const IndicatorComparisonSection: FunctionComponent<IndicatorComparisonSectionProps> = props => {
-  const { location, countryName } = props;
+  const { location, themes, countryName } = props;
+  const [indicatorOne, setIndicatorOne] = useState<SpotlightIndicator | undefined>(undefined);
+  const [indicatorTwo, setIndicatorTwo] = useState<SpotlightIndicator | undefined>(undefined);
+  const onFilterChange = (index: number) => (options: SpotlightOptions): void => {
+    if (options.indicator) {
+      if (index === 0) {
+        setIndicatorOne(options.indicator);
+      } else {
+        setIndicatorTwo(options.indicator);
+      }
+    }
+  };
+  console.log(indicatorOne, indicatorTwo);
 
   return (
     <PageSection dark wide>
       <PageSectionHeading>Compare indicators for {location ? location.name : countryName}</PageSectionHeading>
+      <SpotlightBanner>
+        <SpotlightComparison>
+          <SpotlightFilters
+            themes={themes}
+            onOptionsChange={onFilterChange(0)}
+            topicLabel="Topic"
+            indicatorLabel="Indicator"
+            yearLabel="Year"
+            topicClassName="form-field--inline-three"
+            indicatorClassName="form-field--inline-three"
+            yearClassName="form-field--inline-three"
+          />
+        </SpotlightComparison>
+        <SpotlightComparison legendClassName="spotlight__comparison-legend--alt">
+          <SpotlightFilters
+            themes={themes}
+            onOptionsChange={onFilterChange(1)}
+            topicLabel="Topic"
+            indicatorLabel="Indicator"
+            yearLabel="Year"
+            topicClassName="form-field--inline-three"
+            indicatorClassName="form-field--inline-three"
+            yearClassName="form-field--inline-three"
+          />
+        </SpotlightComparison>
+        <button type="submit" className="button button--compare">
+          Compare
+        </button>
+      </SpotlightBanner>
     </PageSection>
   );
 };
