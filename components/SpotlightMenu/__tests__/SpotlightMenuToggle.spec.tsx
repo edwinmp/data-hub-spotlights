6855a1235367daf86e9c31cab0261d6107d89893
David Ebukali <debukali@gmail.com>
2020-03-02 05:15:41 +0300

    Add unit test for spotlight menu nav and toggle components

---
+++ components/SpotlightMenu/__tests__/SpotlightMenuToggle.spec.tsx
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import * as TestRenderer from 'react-test-renderer';
+import { SpotlightMenuToggle } from '../SpotlightMenuToggle';
+
+describe('SpotlightMenuToggle', () => {
+  test('renders correctly', () => {
+    const renderer = TestRenderer.create(<SpotlightMenuToggle caption="Uganda" />).toJSON();
+
+    expect(renderer).toMatchSnapshot();
+  });
+
+  test('renders children correctly', () => {
+    const renderer = TestRenderer.create(
+      <SpotlightMenuToggle caption="Uganda">
+        <div>Testing</div>
+      </SpotlightMenuToggle>
+    ).toJSON();
+
+    expect(renderer).toMatchSnapshot();
+  });
+
+  test('It adds inactive class when show prop is true', () => {
+    const renderer = TestRenderer.create(<SpotlightMenuToggle caption="Uganda" show={true} />).toJSON();
+
+    expect(renderer).toMatchSnapshot();
+  });
+});
