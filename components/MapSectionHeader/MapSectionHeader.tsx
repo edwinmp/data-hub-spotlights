a48cbd2d0eaabaa56aab4fac73fbcba457de531a
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-03 16:13:48 +0300

    Use AsyncSelect on MapSectionHeader

--- components/MapSectionHeader/MapSectionHeader.tsx
+++ components/MapSectionHeader/MapSectionHeader.tsx
@@ -1,15 +1,19 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, useEffect, useState, CSSProperties } from 'react';
 import { createLocationOptions, getBoundariesByCountryCode, SpotlightBoundary, SpotlightLocation } from '../../utils';
 import { BoundaryMenu } from '../BoundaryMenu';
-import { Select, SelectOption, SelectOptions } from '../Select';
+import { AsyncSelect, SelectOption, SelectOptions } from '../Select';
 import { SpotlightBanner, SpotlightBannerAside, SpotlightBannerMain } from '../SpotlightBanner';
 import { MenuListItem } from '../SpotlightMenu';
+import { ValueType, OptionTypeBase } from 'react-select';
 
 interface MapSectionHeaderProps {
   countryCode: string;
   countryName: string;
   onSelectLocation: (location?: SpotlightLocation) => void;
 }
+
+const noOptionsMessage = (obj: { inputValue: string }): string =>
+  obj.inputValue ? `No results for ${obj.inputValue}` : 'Type to search ...';
 
 const MapSectionHeader: FunctionComponent<MapSectionHeaderProps> = props => {
   const [boundaries, setBoundaries] = useState<SpotlightBoundary[]>([]);
@@ -23,9 +27,13 @@ const MapSectionHeader: FunctionComponent<MapSectionHeaderProps> = props => {
     setOptions(createLocationOptions(boundaries, 'd')); // TODO: allow greater depth when sub-county data comes in
   }, [boundaries]);
 
-  const onSelectLocation = (option?: SelectOption | MenuListItem): void => {
+  const onSelectLocation = (option?: SelectOption | MenuListItem | null): void => {
     props.onSelectLocation(option && option.value ? { geocode: option.value, name: option.label } : undefined);
   };
+  const loadOptions = async (inputValue: string): Promise<SelectOptions> =>
+    options && inputValue
+      ? await options.filter(option => option.label.toLowerCase().includes(inputValue.toLowerCase()))
+      : [];
 
   return (
     <SpotlightBanner header>
@@ -33,14 +41,22 @@ const MapSectionHeader: FunctionComponent<MapSectionHeaderProps> = props => {
         <BoundaryMenu countryName={props.countryName} boundaries={boundaries} onSelectLocation={onSelectLocation} />
       </SpotlightBannerAside>
       <SpotlightBannerMain>
-        <Select
-          options={options}
-          onChange={onSelectLocation}
-          placeholder="Select Location"
-          isLoading={!(options && options.length)}
-          chooseTheme="dark"
-          isClearable
-        />
+        {options && options.length ? (
+          <AsyncSelect
+            loadOptions={loadOptions}
+            placeholder="Search for a location"
+            isLoading={!(options && options.length)}
+            chooseTheme="dark"
+            isClearable
+            defaultOptions
+            styles={{
+              dropdownIndicator: (provided): CSSProperties => ({ ...provided, display: 'none' }),
+              indicatorSeparator: (provided): CSSProperties => ({ ...provided, display: 'none' })
+            }}
+            noOptionsMessage={noOptionsMessage}
+            onChange={onSelectLocation as (options: ValueType<OptionTypeBase>) => void}
+          />
+        ) : null}
       </SpotlightBannerMain>
     </SpotlightBanner>
   );
