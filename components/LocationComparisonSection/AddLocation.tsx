746f1c646e16a53c574c675bfe12e73ae8d97488
David Ebukali <debukali@gmail.com>
2020-03-03 15:42:04 +0300

    Add location component

--- components/LocationComparisonSection/AddLocation.tsx
+++ components/LocationComparisonSection/AddLocation.tsx
@@ -1,12 +1,23 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
 interface AddLocationProps {
   label: string;
+  active?: boolean;
 }
 
-const AddLocation: FunctionComponent<AddLocationProps> = ({ label }) => {
+const AddLocation: FunctionComponent<AddLocationProps> = ({ label, active = true }) => {
+  const [show, setShow] = useState(active);
+  const toggle = (): void => {
+    setShow(false);
+  };
+
   return (
-    <a href="#" className="m-text-link">
+    <a href="#" className="m-text-link" onClick={toggle}>
+      <style jsx>{`
+        .m-text-link {
+          display: ${show ? 'block' : 'none'};
+        }
+      `}</style>
       <i role="presentation" aria-hidden="true" className="ico ico--16 ico-plus-poppy"></i>
       <span>{label}</span>
     </a>
