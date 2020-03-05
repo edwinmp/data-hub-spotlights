7cd7f15b86d25fc1b9d92e5af3b46adb3c4e6f82
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-05 10:54:45 +0300

    Add onAdd property to BaseMapLayer

--- components/BaseMap/BaseMapLayer.tsx
+++ components/BaseMap/BaseMapLayer.tsx
@@ -4,15 +4,19 @@ import { Map, Layer } from 'mapbox-gl';
 interface BaseMapLayerProps extends Layer {
   map?: Map;
   show?: boolean;
+  onAdd?: (map: Map, layerID: string) => void;
 }
 
-const BaseMapLayer: FunctionComponent<BaseMapLayerProps> = ({ map, show, ...options }) => {
+const BaseMapLayer: FunctionComponent<BaseMapLayerProps> = ({ map, show, onAdd, ...options }) => {
   if (map) {
     if (map.getLayer(options.id)) {
       map.removeLayer(options.id);
     }
     if (show) {
       map.addLayer(options);
+      if (onAdd) {
+        onAdd(map, options.id);
+      }
     }
   }
 
