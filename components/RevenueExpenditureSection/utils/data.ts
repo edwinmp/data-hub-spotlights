2f6e7b1ddfda3e03fae9c1562605098215bdacdd
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-12 12:21:32 +0300

    Update grouping to by year & budgetType

--- components/RevenueExpenditureSection/utils/data.ts
+++ components/RevenueExpenditureSection/utils/data.ts
@@ -3,6 +3,7 @@ import { RevenueExpenditureData } from './types';
 
 export const processRevenueExpenditureData = (data: LocationData[]): RevenueExpenditureData[] => {
   return data.map(_data => ({
+    year: _data.year,
     value: _data.value,
     valueLocalCurrency: extraValueFromMeta(_data.meta, 'valueLocalCurrency') as number,
     budgetType: extraValueFromMeta(_data.meta, 'budgetType') as BudgetType
