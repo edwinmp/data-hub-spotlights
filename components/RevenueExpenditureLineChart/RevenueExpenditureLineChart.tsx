d72d4ae1a36f0d8220ed2aa4cdff7570e8df2d22
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-12 17:28:31 +0300

    Reconfigure Line Chart with value XAxis

--- components/RevenueExpenditureLineChart/RevenueExpenditureLineChart.tsx
+++ components/RevenueExpenditureLineChart/RevenueExpenditureLineChart.tsx
@@ -1,18 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import { EChartsBaseChart } from '../EChartsBaseChart';
-// import { toBasicAxisData } from '../EChartsBaseChart/utils';
-import { RevenueExpenditureData } from '../RevenueExpenditureSection/utils';
+import { YearData } from '../RevenueExpenditureSection/utils';
 
 interface ComponentProps {
-  data?: RevenueExpenditureData[];
+  data: YearData;
   height?: string;
 }
 
 const RevenueExpenditureLineChart: FunctionComponent<ComponentProps> = props => {
-  if (!props.data) {
-    return <div>No Data</div>;
-  }
-
   const options: ECharts.Options = {
     legend: { show: false },
     tooltip: {
@@ -22,25 +17,17 @@ const RevenueExpenditureLineChart: FunctionComponent<ComponentProps> = props => 
       },
       formatter: (params: ECharts.TooltipFormatterParams[]): string => {
         const { value } = params[0];
-        if (value) {
-          const date = new Date(value[0]);
 
-          return `<div style="font-size:16px;"><strong>${date.getFullYear()}</strong> - ${value[1]}</div>`;
-        }
-
-        return `<em>tooltip</em>`;
+        return `<div style="font-size:16px;"><strong>${value[0]}</strong> - ${value[1]}</div>`;
       }
     },
     xAxis: {
-      type: 'time',
-      min: '2001-01-01',
+      type: 'value',
+      min: 2000,
       axisLabel: {
-        formatter: (value): number => {
-          const date = new Date(value);
-
-          return date.getFullYear();
-        }
-      }
+        formatter: (value: number): number => value
+      },
+      interval: 1
     },
     yAxis: {
       splitLine: { show: true }
@@ -49,12 +36,8 @@ const RevenueExpenditureLineChart: FunctionComponent<ComponentProps> = props => 
       {
         type: 'line',
         data: [
-          ['2001-01-01', 20],
-          ['2002-01-01', 34],
-          ['2003-01-01', 45],
-          ['2004-01-01', 23],
-          ['2005-01-01', 12],
-          ['2006-01-01', 7]
+          [2001, 20],
+          [2002, 34]
         ],
         showSymbol: false
       }
