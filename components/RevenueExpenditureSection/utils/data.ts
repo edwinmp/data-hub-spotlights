05b994f2395db3dac9590f639a5921531ee90991
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-12 13:05:46 +0300

    Filter out null value data

--- components/RevenueExpenditureSection/utils/data.ts
+++ components/RevenueExpenditureSection/utils/data.ts
@@ -2,10 +2,12 @@ import { BudgetType, extraValueFromMeta, LocationData } from '../../../utils';
 import { RevenueExpenditureData } from './types';
 
 export const processRevenueExpenditureData = (data: LocationData[]): RevenueExpenditureData[] => {
-  return data.map(_data => ({
-    year: _data.year,
-    value: _data.value,
-    valueLocalCurrency: extraValueFromMeta(_data.meta, 'valueLocalCurrency') as number,
-    budgetType: extraValueFromMeta(_data.meta, 'budgetType') as BudgetType
-  }));
+  return data
+    .filter(_data => _data.value)
+    .map(_data => ({
+      year: _data.year,
+      value: _data.value,
+      valueLocalCurrency: extraValueFromMeta(_data.meta, 'valueLocalCurrency') as number,
+      budgetType: extraValueFromMeta(_data.meta, 'budgetType') as BudgetType
+    }));
 };
