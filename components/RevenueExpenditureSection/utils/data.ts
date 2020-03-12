a8e0596cc9cf809b106373395c5903dfc45c2b83
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-12 13:53:33 +0300

    Extract revenue & expenditure levels

--- components/RevenueExpenditureSection/utils/data.ts
+++ components/RevenueExpenditureSection/utils/data.ts
@@ -1,6 +1,18 @@
 import { BudgetType, extraValueFromMeta, LocationData } from '../../../utils';
 import { RevenueExpenditureData } from './types';
 
+const getLevels = (data: LocationData): string[] => {
+  const levels: string[] = [];
+  ['l1', 'l2', 'l3', 'l4', 'l5', 'l6'].forEach(level => {
+    const value = extraValueFromMeta(data.meta, level) as string;
+    if (value) {
+      levels.push(value);
+    }
+  });
+
+  return levels;
+};
+
 export const processRevenueExpenditureData = (data: LocationData[]): RevenueExpenditureData[] => {
   return data
     .filter(_data => _data.value)
@@ -8,6 +20,7 @@ export const processRevenueExpenditureData = (data: LocationData[]): RevenueExpe
       year: _data.year,
       value: _data.value,
       valueLocalCurrency: extraValueFromMeta(_data.meta, 'valueLocalCurrency') as number,
-      budgetType: extraValueFromMeta(_data.meta, 'budgetType') as BudgetType
+      budgetType: extraValueFromMeta(_data.meta, 'budgetType') as BudgetType,
+      levels: getLevels(_data)
     }));
 };
--- components/RevenueExpenditureSection/utils/types.ts
+++ components/RevenueExpenditureSection/utils/types.ts
@@ -16,4 +16,5 @@ export interface RevenueExpenditureData {
   value: number;
   valueLocalCurrency: number;
   budgetType: BudgetType;
+  levels: string[];
 }
