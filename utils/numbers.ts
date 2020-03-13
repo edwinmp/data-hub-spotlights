3440a5e50cc28b51229cb71a944a43e3d2ce8f34
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-13 10:52:02 +0300

    Add formatter for values over a trillion

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -1,4 +1,7 @@
 export const formatNumber = (value: number, decimals = 2): string => {
+  if (value >= 1000000000000) {
+    return `${(value / 1000000000000).toFixed(decimals)}tn`;
+  }
   if (value >= 1000000000) {
     return `${(value / 1000000000).toFixed(decimals)}bn`;
   }
