7c425d140fd251d9f90c85e45049406f0b6d21a0
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-17 12:17:50 +0300

    Extract getIndicatorContentOptions

--- components/RevenueExpenditureSection/utils/hooks.ts
+++ components/RevenueExpenditureSection/utils/hooks.ts
@@ -1,22 +1,13 @@
 import { groupBy } from 'underscore';
+import { SpotlightIndicator } from '../../../utils';
 import { DataLoaderProps, useDDWData } from '../../DDWDataLoader';
-import { processRevenueExpenditureData } from './data';
+import { getIndicatorContentOptions, processRevenueExpenditureData } from './data';
 import { BudgetTypeData, RevenueExpenditureHook as REHook } from './types';
-import { SpotlightIndicator, SpotlightIndicatorContent } from '../../../utils';
 
 export const useRevenueExpenditureData = (optns: DataLoaderProps, indicator: SpotlightIndicator): REHook => {
   const { data, dataLoading, options, setOptions } = useDDWData(optns);
-  let contentOptions: SpotlightIndicatorContent = {};
-  if (indicator.content_template) {
-    try {
-      contentOptions = JSON.parse(indicator.content_template);
-    } catch (error) {
-      console.log('Invalid JSON:', error.message);
-    }
-  }
-
   if (!dataLoading && data && data.length) {
-    const configs = contentOptions.revenue || contentOptions.expenditure;
+    const configs = getIndicatorContentOptions(indicator);
     const processedData = processRevenueExpenditureData(data[0].data, configs);
     const groupedByYear = groupBy(processedData, processedData => processedData.year);
     const groupedByYearAndBudgetType: { [key: string]: BudgetTypeData } = {};
