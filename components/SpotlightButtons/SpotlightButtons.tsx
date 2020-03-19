bbb9edc0028eaabedb499f88ea30ce288f42b348
David Ebukali <debukali@gmail.com>
2020-03-19 17:44:19 +0300

    Add spotlight buttons div for share and compare buttons

---
+++ components/SpotlightButtons/SpotlightButtons.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const SpotlightButtons: FunctionComponent = props => {
+  return <div className="spotlight-buttons">{props.children}</div>;
+};
+
+export { SpotlightButtons };
