cd1f0fb0bde183f45736b8fc32acbe7dd1684cfe
David Ebukali <debukali@gmail.com>
2020-03-20 22:39:18 +0300

    Increase api limit to 10000 and set null data values to 0 for the chart

--- components/LocationComparisonSection/LocationFiltersAndCharts.tsx
+++ components/LocationComparisonSection/LocationFiltersAndCharts.tsx
@@ -31,8 +31,7 @@ const LocationFiltersAndCharts: FunctionComponent<LocationFiltersAndChartsProps>
     }
   };
   const onLoad = (): void => setLoading(false);
-  useEffect(() => setLoading(true), [selectedLocations]);
-  useEffect(() => setLoading(true), [selections]);
+  useEffect(() => setLoading(true), [selectedLocations, selections]);
 
   return show ? (
     <>
