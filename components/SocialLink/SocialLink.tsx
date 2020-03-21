6e4bb75df6bcd60d5bdac6dc15c61d514a7fcd02
mariamiah <mariah.kakande@gmail.com>
2020-03-21 14:19:48 +0300

    use theme classes

---
+++ components/SocialLink/SocialLink.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SocialLink = (props: any) => {
+  const handleClick = () => {
+    console.log('clicked');
+  };
+  return (
+    <a href={props.url}>
+      <img src={'/assets/svg/source/' + props.socialSource + '.svg'} alt={props.socialSource} onClick={handleClick} />
+      <style jsx>{`
+        img {
+          float: left;
+          margin: 0 5px;
+          height: 3em;
+          width: 3em;
+        }
+      `}</style>
+    </a>
+  );
+};
+
+export { SocialLink };
--- components/SpotlightShare/SpotlightShare.tsx
+++ components/SpotlightShare/SpotlightShare.tsx
@@ -1,93 +1,64 @@
 import React, { FunctionComponent, useState } from 'react';
 import Modal from 'react-modal';
-import styled from 'styled-components';
 import { Button } from '../Button';
-import { SVG } from './icon';
+import { SocialLink } from '../SocialLink';
 
 interface SpotlightShareProps {
   maxHeight?: string;
   minHeight?: string;
   className?: string;
 }
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    maxWidth: '680px',
-    height: '280px',
-    maxHeight: '100%',
-    padding: '20px',
-    overflow: 'auto',
-    borderRadius: '8px',
-    backgroundColor: '#fefefe',
-    boxShadow: '20px 40px 50px rgba(0, 0, 0, 0.1)'
-  },
-  overlay: {
-    background: 'rgba(0,0,0,.85)',
-    zIndex: 1000
-  }
-};
 
-const Title = styled.h2`
-  font-size: 2.6rem;
-`;
-const ToggleButton = styled.button`
-  cursor: pointer;
-  background: #000;
-  font-size: 26px;
-  border-bottom-left-radius: 8px;
-  color: #fff;
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 0px 15px;
-  display: block;
-`;
-const TextInput = styled.input`
-  padding: 2em 1.5em;
-  height: 5em;
-`;
-
-const RadioInput = styled.input`
-  margin-right: 1em !important;
-`;
-const SpotlightShare: FunctionComponent<SpotlightShareProps> = props => {
+const SpotlightShare: FunctionComponent<SpotlightShareProps> = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <div className={props.className}>
+    <div>
       <Button onClick={toggleModal}>Share visualisation</Button>
       <Modal
         isOpen={isOpen}
         onRequestClose={toggleModal}
-        style={customStyles}
         ariaHideApp={false}
         shouldCloseOnOverlayClick={false}
+        className="modal__content modal__content--minor"
+        overlayClassName="modal modal--share modal--share-open"
       >
-        <form>
-          <Title>Share this visualisation</Title>
+        <form className="form">
+          <h2 className="modal__heading">Share this visualisation</h2>
           <br />
-          <label htmlFor="one">
-            <RadioInput type="radio" id="one" name="first_item" value="1" />
+          <label htmlFor="one" className="form-label">
+            <input type="radio" id="one" name="first_item" value="1" className="form-field__radio-buttons form-radio" />
             In default view
           </label>
-          <label htmlFor="two">
-            <RadioInput type="radio" id="two" name="second_item" value="2" />
+          <label htmlFor="two" className="form-label">
+            <input
+              type="radio"
+              id="two"
+              name="second_item"
+              value="2"
+              className="form-field__radio-buttons form-radio"
+            />
             As I configured it
           </label>
           <br />
-          <TextInput type="text" id="urllink" name="urllink" />
+          <input className="form-item" type="text" id="urllink" name="urllink" />
           <br />
           <br />
-          <SVG socialSource="twitter" />
-          <SVG socialSource="facebook" />
-          <SVG socialSource="email" />
+          <SocialLink socialSource="twitter" url="https://twitter.com" />
+          <SocialLink socialSource="facebook" url="https://facebook.com" />
+          <SocialLink socialSource="email" url="mailto:" />
         </form>
-        <ToggleButton onClick={toggleModal}>x</ToggleButton>
+        <button className="modal-button-close js-modal-trigger" onClick={toggleModal}>
+          x
+        </button>
       </Modal>
+      <style jsx>{`
+        input[type='radio'] {
+          margin-right: 1em !important;
+        }
+      `}</style>
     </div>
   );
 };
