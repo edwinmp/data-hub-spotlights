6470c7f54951d76b7ea791d0ff6ca833636b500f
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-24 13:38:57 +0300

    Refactor Button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,19 +1,11 @@
 import React, { FunctionComponent } from 'react';
 
-interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
-  className?: string;
-  onClick?: () => void;
-  show?: boolean;
-}
+type ButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
-const Button: FunctionComponent<ButtonProps> = ({ children, className, onClick, show = true }) => {
-  return (
-    <button type="button" className={className} onClick={onClick}>
-      <style jsx>{`
-        display: ${show ? 'block' : 'none'};
-      `}</style>
-      {children}
-    </button>
-  );
+const Button: FunctionComponent<ButtonProps> = ({ children, ...props }) => {
+  return <button {...props}>{children}</button>;
 };
+
+Button.defaultProps = { type: 'button', className: 'button' };
+
 export { Button };
