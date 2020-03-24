7bdbf4fc458156e0ecc7369994dcf8cb2722a173
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-24 20:01:25 +0300

    Complete LocationComparison chart refactor

--- components/LocationComparisonChartDataHandler/LocationComparisonChartDataHandler.tsx
+++ components/LocationComparisonChartDataHandler/LocationComparisonChartDataHandler.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { LocationIndicatorData, SpotlightLocation, LocationData } from '../../utils';
 import { LocationComparisonLineChart } from '../LocationComparisonLineChart';
+import { groupBy } from 'underscore';
 
 interface ComponentProps {
   data?: LocationIndicatorData;
@@ -8,55 +9,10 @@ interface ComponentProps {
   countryCode: string;
 }
 
-const getChartData = (data: LocationData[], locations: SpotlightLocation[]): any => {
-  const chartDataArray: any = [];
-  for (const k in data) {
-    if (data.hasOwnProperty(k)) {
-      const { name, value, year } = data[k];
-      for (const key in locations) {
-        if (locations.hasOwnProperty(key)) {
-          const element = locations[key];
-          if (name.toLowerCase() === element.name.toLowerCase()) {
-            chartDataArray.push({
-              name,
-              value: value ? value : 0,
-              year
-            });
-          }
-        }
-      }
-    }
-  }
-  return chartDataArray;
-};
-
-const getSeriesData = (chartData: any, locations: any): [] => {
-  const seriesDataArray: any = [];
-  for (const k in locations) {
-    const seriesData: any = {};
-    seriesData['data'] = [];
-    seriesData['year'] = [];
-    if (locations.hasOwnProperty(k)) {
-      for (const key in chartData) {
-        if (chartData.hasOwnProperty(key)) {
-          if (locations[k].name.toLowerCase() === chartData[key]['name'].toLowerCase()) {
-            seriesData['name'] = chartData[key]['name'];
-            seriesData['type'] = 'line';
-            seriesData['data'].push(chartData[key]['value']);
-            seriesData['year'].push(chartData[key]['year']);
-          }
-        }
-      }
-      seriesDataArray.push(seriesData);
-    }
-  }
-  return seriesDataArray;
-};
-
 const getYears = (data: LocationData[]): number[] =>
-  data.reduce((prev: number[], curr) => (prev.indexOf(curr.year) === -1 ? prev.concat(curr.year) : prev), []);
+  data.reduce((prev: number[], curr) => (prev.indexOf(curr.year) === -1 ? prev.concat(curr.year) : prev), []).sort();
 
-const LocationComparisonChartDataHandler: FunctionComponent<ComponentProps> = ({ data, ...props }) => {
+const LocationComparisonChartDataHandler: FunctionComponent<ComponentProps> = ({ data }) => {
   if (!data) {
     return (
       <div>
@@ -69,10 +25,14 @@ const LocationComparisonChartDataHandler: FunctionComponent<ComponentProps> = ({
     );
   }
 
-  const chartData = getChartData(data.data, props.locations);
-  const seriesData = getSeriesData(chartData, props.locations);
+  const groupedByLocation: { [location: string]: LocationData[] } = groupBy(data.data, data => data.name);
+  const groupedByYear: { [location: string]: { [year: string]: LocationData[] } } = {};
+  Object.keys(groupedByLocation).forEach(location => {
+    const groupedByBudgetType = groupBy(groupedByLocation[location], processedData => processedData.year);
+    groupedByYear[location] = groupedByBudgetType;
+  });
 
-  return <LocationComparisonLineChart years={getYears(data.data)} series={seriesData} height={'500px'} />;
+  return <LocationComparisonLineChart years={getYears(data.data)} data={groupedByYear} height={'500px'} />;
 };
 
 export { LocationComparisonChartDataHandler };
