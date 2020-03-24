e9857e4da6ab8445be5aeb05f183ff63f70d7204
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-24 20:01:25 +0300

    Complete LocationComparison chart refactor

--- components/LocationComparisonLineChart/LocationComparisonLineChart.tsx
+++ components/LocationComparisonLineChart/LocationComparisonLineChart.tsx
@@ -2,28 +2,36 @@ import React, { FunctionComponent } from 'react';
 import { EChartOption } from 'echarts';
 import { EChartsBaseChart } from '../EChartsBaseChart';
 import { toBasicAxisData } from '../EChartsBaseChart/utils';
+import { LocationData } from '../../utils';
 
 interface ComponentProps {
   years: (string | number)[];
-  series: [];
+  data: FormatedData;
   height?: string;
 }
 
+type FormatedData = { [location: string]: { [year: string]: LocationData[] } };
+
 const LocationComparisonLineChart: FunctionComponent<ComponentProps> = props => {
-  const options: EChartOption = {
+  const options: EChartOption<EChartOption.SeriesLine | EChartOption.SeriesBar> = {
     tooltip: {},
     legend: { show: true },
     xAxis: {
-      data: toBasicAxisData(props.years ? props.years : [])
+      data: toBasicAxisData(props.years ? props.years : []),
+      interval: props.years.length <= 12 ? 1 : 4
     },
-    series: props.series.map(({ name, data }) => ({
-      name,
-      type: 'line',
-      data
+    series: Object.keys(props.data).map<EChartOption.SeriesLine | EChartOption.SeriesBar>(location => ({
+      name: location,
+      data: Object.keys(props.data[location]).map(year => props.data[location][year][0].value),
+      type: props.years.length > 2 ? 'line' : 'bar',
+      connectNulls: true,
+      barWidth: 40
     }))
   };
 
-  return <EChartsBaseChart options={options} height={'500px'} />;
+  return <EChartsBaseChart options={options} height={props.height} />;
 };
+
+LocationComparisonLineChart.defaultProps = { height: '500px' };
 
 export { LocationComparisonLineChart };
