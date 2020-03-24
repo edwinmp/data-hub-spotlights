d3fa875f04a70fb92c6c30132c7d64c9d441378a
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-24 12:28:48 +0300

    Add selectStyles prop to selection banner

--- components/LocationSelectionBanner/LocationSelectionBanner.tsx
+++ components/LocationSelectionBanner/LocationSelectionBanner.tsx
@@ -4,13 +4,14 @@ import { BoundaryMenu } from '../BoundaryMenu';
 import { AsyncSelect, SelectOption, SelectOptions } from '../Select';
 import { SpotlightBanner, SpotlightBannerAside, SpotlightBannerMain } from '../SpotlightBanner';
 import { MenuListItem } from '../SpotlightMenu';
-import { ValueType, OptionTypeBase } from 'react-select';
+import { ValueType, OptionTypeBase, Styles } from 'react-select';
 
 interface LocationSelectionBannerProps {
   countryCode: string;
   countryName: string;
   className?: string;
   onSelectLocation: (location?: SpotlightLocation) => void;
+  selectStyles?: Partial<Styles>;
 }
 
 const noOptionsMessage = (obj: { inputValue: string }): string =>
@@ -42,24 +43,30 @@ const LocationSelectionBanner: FunctionComponent<LocationSelectionBannerProps> =
         <BoundaryMenu countryName={props.countryName} boundaries={boundaries} onSelectLocation={onSelectLocation} />
       </SpotlightBannerAside>
       <SpotlightBannerMain>
-        {options && options.length ? (
-          <AsyncSelect
-            loadOptions={loadOptions}
-            placeholder="Search for a location"
-            isLoading={!(options && options.length)}
-            chooseTheme="dark"
-            isClearable
-            defaultOptions
-            styles={{
-              dropdownIndicator: (provided): CSSProperties => ({ ...provided, display: 'none' }),
-              indicatorSeparator: (provided): CSSProperties => ({ ...provided, display: 'none' }),
-              singleValue: (provided): CSSProperties => ({ ...provided, textTransform: 'capitalize' })
-            }}
-            noOptionsMessage={noOptionsMessage}
-            onChange={onSelectLocation as (options: ValueType<OptionTypeBase>) => void}
-          />
-        ) : null}
-        {props.children}
+        <div>
+          {options && options.length ? (
+            <AsyncSelect
+              loadOptions={loadOptions}
+              placeholder="Search for a location"
+              isLoading={!(options && options.length)}
+              chooseTheme="dark"
+              isClearable
+              defaultOptions
+              styles={{
+                dropdownIndicator: (provided): CSSProperties => ({ ...provided, display: 'none' }),
+                indicatorSeparator: (provided): CSSProperties => ({ ...provided, display: 'none' }),
+                singleValue: (provided): CSSProperties => ({ ...provided, textTransform: 'capitalize' }),
+                ...props.selectStyles
+              }}
+              noOptionsMessage={noOptionsMessage}
+              onChange={onSelectLocation as (options: ValueType<OptionTypeBase>) => void}
+            />
+          ) : null}
+          {props.children}
+          <style jsx>{`
+            display: flex;
+          `}</style>
+        </div>
       </SpotlightBannerMain>
     </SpotlightBanner>
   );
