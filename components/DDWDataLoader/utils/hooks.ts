c5055202509bf05fc053933a931788571cb6936b
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-31 10:37:27 +0300

    Refactor useDDWData custom hook

--- components/DDWDataLoader/utils/hooks.ts
+++ components/DDWDataLoader/utils/hooks.ts
@@ -2,26 +2,28 @@ import { useQuery } from '@apollo/react-hooks';
 import { ApolloError } from 'apollo-client';
 import { Dispatch, SetStateAction, useState } from 'react';
 import { DataLoaderProps } from '..';
-import { GET_INDICATOR_DATA, LocationIndicatorData } from '../../../utils';
+import { GET_INDICATOR_DATA, LocationIndicatorData, DataFilter } from '../../../utils';
 
 interface DDWData {
   data?: LocationIndicatorData[];
   dataLoading: boolean;
   options: DataLoaderProps;
+  filter?: DataFilter[][];
   setOptions: Dispatch<SetStateAction<DataLoaderProps>>;
   error?: ApolloError;
 }
 
 export const useDDWData = (_options: DataLoaderProps): DDWData => {
   const [options, setOptions] = useState(_options);
-  const { indicators, geocodes, startYear, endYear, limit } = options;
+  const { indicators, geocodes, startYear, endYear, filter, limit } = options;
   if (indicators && indicators.length) {
     const { data, loading, error } = useQuery<{ data: LocationIndicatorData[] }>(GET_INDICATOR_DATA, {
       variables: {
         indicators,
-        geocodes: geocodes || [],
+        geocodes,
         startYear,
-        endYear: endYear || startYear,
+        endYear,
+        filter,
         limit
       }
     });
