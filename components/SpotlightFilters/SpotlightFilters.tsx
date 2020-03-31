7f7b5f421643dcb6f5a54f68448efc656ad8ef05
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-31 22:03:45 +0300

    Refactor SpotlightFilters

--- components/SpotlightFilters/SpotlightFilters.tsx
+++ components/SpotlightFilters/SpotlightFilters.tsx
@@ -3,19 +3,17 @@ import React, { FunctionComponent, useEffect, useState } from 'react';
 import {
   createYearOptionsFromIndicator,
   getDefaultsByIndex,
+  getDefaultsFromQuery,
+  getOptionByIndexOrValue,
   getThemeDefaultsByIndex,
-  INDICATOR_QUERY,
   parseIndicatorToOption,
   SpotlightOptions,
-  THEME_QUERY,
-  YEAR_QUERY,
-  getDefaultsFromQuery,
-  getOptionByIndexOrValue
+  THEME_QUERY
 } from '../../utils';
 import { FormField } from '../FormField';
 import { Select, SelectOption } from '../Select';
 import IndicatorFilterForm from './IndicatorFilterForm';
-import { defaultSelectOptions, FilterSelectOptions, SpotlightFilterProps } from './utils';
+import { defaultSelectOptions, FilterSelectOptions, setQuery, SpotlightFilterProps } from './utils';
 
 const SpotlightFilters: FunctionComponent<SpotlightFilterProps> = ({ defaultIndexes, ...props }) => {
   const router = useRouter();
@@ -27,6 +25,7 @@ const SpotlightFilters: FunctionComponent<SpotlightFilterProps> = ({ defaultInde
   const [selected, setSelected] = useState<SpotlightOptions>(defaultSelected);
   const { theme: activeTheme, indicator: activeIndicator, year: activeYear } = selected;
 
+  useEffect(() => setQuery(router, selected), []);
   useEffect(() => props.onOptionsChange(selected), [selected]);
 
   const onSelectTheme = (option?: SelectOption): void => {
@@ -36,19 +35,12 @@ const SpotlightFilters: FunctionComponent<SpotlightFilterProps> = ({ defaultInde
         const { options: themeOptions, selected: themeSelected } = getThemeDefaultsByIndex(selectedTheme, options);
         setSelected(themeSelected);
         setOptions(themeOptions);
-        const href = router.route;
-        const as = router.asPath + `?${THEME_QUERY}=${themeSelected.theme?.slug}`;
-        if (router.asPath == `spotlight/${router.query.slug}`) {
-          router.push(href, as, { shallow: true });
-        } else {
-          const asPath = router.asPath.split(/\?/)[0];
-          const as = asPath + `?${THEME_QUERY}=${option.value}`;
-          router.push(href, as, { shallow: true });
-        }
+        setQuery(router, themeSelected);
       }
     } else if (activeIndicator) {
       setSelected({});
       setOptions(defaultSelectOptions);
+      setQuery(router, defaultSelected);
     }
   };
 
@@ -56,55 +48,29 @@ const SpotlightFilters: FunctionComponent<SpotlightFilterProps> = ({ defaultInde
     if (option && activeTheme) {
       const selectedIndicator = activeTheme.indicators.find(indicator => indicator.ddw_id === option.value);
       const yearOptions = selectedIndicator ? createYearOptionsFromIndicator(selectedIndicator) : undefined;
-      setSelected({
+      const _selected = {
         ...selected,
         indicator: selectedIndicator,
         year: yearOptions && parseInt(yearOptions[0].value, 10)
-      });
+      };
+      setSelected(_selected);
       setOptions({ ...options, years: yearOptions });
-      if (router.asPath.indexOf(`?${THEME_QUERY}=`) > 0) {
-        const asPath = router.asPath.split(/\?/)[1];
-        if (!asPath.includes(INDICATOR_QUERY)) {
-          const as = router.asPath + `&${INDICATOR_QUERY}=${option.value}`;
-          router.push(router.route, as, { shallow: true });
-        } else {
-          let urlParts = router.asPath.split('&');
-          urlParts = urlParts.filter(e => !e.startsWith(INDICATOR_QUERY));
-          const newUrl = urlParts.join('&');
-          const as = newUrl + `&${INDICATOR_QUERY}=${option.value}`;
-          router.push(router.route, as, { shallow: true });
-        }
-      } else {
-        router.push(
-          router.route,
-          router.asPath + `?${THEME_QUERY}=${activeTheme.slug}` + `&${INDICATOR_QUERY}=${option.value}`,
-          { shallow: true }
-        );
-      }
+      setQuery(router, _selected);
     } else if (activeIndicator) {
       setSelected({ ...selected, indicator: undefined, year: undefined });
       setOptions({ ...options, years: [] });
+      setQuery(router, { ...selected, indicator: undefined, year: undefined });
     }
   };
 
   const onSelectYear = (option?: SelectOption): void => {
     if (option && option.value) {
-      setSelected({ ...selected, year: parseInt(option.value, 10) });
-      if (router.asPath.indexOf(`?${THEME_QUERY}=`) > 0) {
-        let urlParts = router.asPath.split('&');
-        urlParts = urlParts.filter(e => !e.startsWith(YEAR_QUERY));
-        const newUrl = urlParts.join('&');
-        const as = newUrl + `&${YEAR_QUERY}=${option.value}`;
-        router.push(router.route, as, { shallow: true });
-      } else {
-        router.push(
-          router.route,
-          router.asPath + `?${THEME_QUERY}=${activeTheme?.slug}` + `&${YEAR_QUERY}=${option.value}`,
-          { shallow: true }
-        );
-      }
+      const _selected = { ...selected, year: parseInt(option.value, 10) };
+      setSelected(_selected);
+      setQuery(router, _selected);
     } else {
       setSelected({ ...selected, year: undefined });
+      setQuery(router, { ...selected, year: undefined });
     }
   };
 
--- utils/themes.ts
+++ utils/themes.ts
@@ -1,8 +1,8 @@
 import { SpotlightTheme } from '.';
 
-export const THEME_QUERY = 'mapTopic';
-export const INDICATOR_QUERY = 'mapIndicator';
-export const YEAR_QUERY = 'mapYear';
+export const THEME_QUERY = 'mt'; // map topic
+export const INDICATOR_QUERY = 'mi'; // map indicator
+export const YEAR_QUERY = 'my'; // map year
 
 export const filterThemesBySection = (themes: SpotlightTheme[], section: string): SpotlightTheme[] =>
   themes.filter(theme => theme.section === section);
