46655064e1b39162510ba09e3cb0609ff8e9613a
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-03-31 19:37:59 +0300

    Fix SpotlightShare

--- components/SpotlightShare/SpotlightShare.tsx
+++ components/SpotlightShare/SpotlightShare.tsx
@@ -4,6 +4,9 @@ import { SpotlightLocation, toCamelCase } from '../../utils';
 import { AnchorButton } from '../AnchorButton';
 import { SocialLink } from '../SocialLink';
 import { getShortUrl } from './utils';
+import { Button } from '../Button';
+import { FormField } from '../FormField';
+import { FormFieldRadio, FormFieldRadioGroup } from '../FormFieldRadio';
 
 interface SpotlightShareProps {
   buttonCaption?: string;
@@ -14,6 +17,7 @@ interface SpotlightShareProps {
 const SpotlightShare: FunctionComponent<SpotlightShareProps> = ({ buttonCaption, ...props }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [url, setUrl] = useState('');
+  const [radioValue, setRadioValue] = useState('default');
   const toggleModalOpen = (): void => {
     if (!modalOpen) {
       getShortUrl()
@@ -21,6 +25,9 @@ const SpotlightShare: FunctionComponent<SpotlightShareProps> = ({ buttonCaption,
         .catch(error => console.log('Error while generating short URL: ', error.message));
     }
     setModalOpen(!modalOpen);
+  };
+  const onSelectOption = (value: string): void => {
+    setRadioValue(value);
   };
 
   return (
@@ -33,45 +40,60 @@ const SpotlightShare: FunctionComponent<SpotlightShareProps> = ({ buttonCaption,
         shouldCloseOnOverlayClick={false}
         className="modal__content modal__content--minor"
         overlayClassName="modal modal--share modal--share-open"
+        style={{ content: { outline: 'none' } }}
       >
+        <h2 className="modal__heading">Share this visualisation</h2>
         <form className="form">
-          <h2 className="modal__heading">Share this visualisation</h2>
-          <br />
-          <label htmlFor="one" className="form-label">
-            <input type="radio" id="one" name="first_item" value="1" className="form-field__radio-buttons form-radio" />
-            In default view
-          </label>
-          <label htmlFor="two" className="form-label">
-            <input
-              type="radio"
-              id="two"
-              name="second_item"
-              value="2"
-              className="form-field__radio-buttons form-radio"
-            />
-            As I configured it
-          </label>
-          <br />
-          <input className="form-item" type="text" id="urllink" name="urllink" value={url} />
-          <br />
-          <br />
-          <SocialLink socialSource="twitter" url={'https://twitter.com/intent/tweet?text=' + url} />
-          <SocialLink socialSource="facebook" url={'https://facebook.com/share.php?u=' + url} />
-          <SocialLink
-            socialSource="email"
-            url={
-              'mailto:?subject=Development Initiatives: ' +
-              toCamelCase(props.location ? props.location.name : props.countryName) +
-              '&body=Development Initiatives:' +
-              toCamelCase(props.location ? props.location.name : props.countryName) +
-              '%0A%0A' +
-              url
-            }
-          />
+          <FormField className="">
+            <div className="form-label form-label--hidden">Share</div>
+            <FormFieldRadio>
+              <FormFieldRadioGroup
+                label="In default view"
+                value="default"
+                name="default"
+                checked={radioValue === 'default'}
+                onClick={onSelectOption}
+              />
+              <FormFieldRadioGroup
+                label="As I configured it"
+                value="asConfigured"
+                name="asConfigured"
+                checked={radioValue === 'asConfigured'}
+                onClick={onSelectOption}
+              />
+            </FormFieldRadio>
+          </FormField>
+
+          <FormField className="">
+            <div className="form-label form-label--hidden">url</div>
+            <input className="form-item" type="text" id="urllink" name="urllink" value={url} />
+          </FormField>
+
+          <ul className="footer__social">
+            <li>
+              <SocialLink socialSource="twitter" url={'https://twitter.com/intent/tweet?text=' + url} />
+            </li>
+            <li>
+              <SocialLink socialSource="facebook" url={'https://facebook.com/share.php?u=' + url} />
+            </li>
+            <li>
+              <SocialLink
+                socialSource="email"
+                url={
+                  'mailto:?subject=Development Initiatives: ' +
+                  toCamelCase(props.location ? props.location.name : props.countryName) +
+                  '&body=Development Initiatives:' +
+                  toCamelCase(props.location ? props.location.name : props.countryName) +
+                  '%0A%0A' +
+                  url
+                }
+              />
+            </li>
+          </ul>
         </form>
-        <button className="modal-button-close js-modal-trigger" onClick={toggleModalOpen}>
-          x
-        </button>
+        <Button className="modal-button-close" onClick={toggleModalOpen}>
+          ×
+        </Button>
       </Modal>
       <style jsx>{`
         input[type='radio'] {
