cad48f677d97d92649daaf4874ae85799169aee0
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-04-20 14:53:50 +0300

    Deprecate LocationComparisonWrapper

--- components/LocationComparisonSection/LocationComparisonChartSection.tsx
+++ components/LocationComparisonSection/LocationComparisonChartSection.tsx
@@ -1,10 +1,16 @@
 import { useRouter } from 'next/router';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { getDefaultsByIndex, SpotlightLocation, SpotlightOptions, SpotlightTheme } from '../../utils';
+import { Button } from '../Button';
+import { FormField } from '../FormField';
+import { Icon } from '../Icon';
+import { LocationComparisonChartDataHandler } from '../LocationComparisonChartDataHandler';
 import { setLocationsQuery } from '../MapSection/utils';
 import { PageSection } from '../PageSection';
 import { Spotlight } from '../Spotlight';
-import { LocationComparisonWrapper } from './LocationComparisonWrapper';
+import { SpotlightBanner } from '../SpotlightBanner';
+import { SpotlightFilters } from '../SpotlightFilters';
+import { VisualisationSectionMain } from '../VisualisationSection';
 
 interface ComponentProps {
   themes: SpotlightTheme[];
@@ -22,21 +28,42 @@ const LocationComparisonChartSection: FunctionComponent<ComponentProps> = props 
   }, [props.locations]);
 
   const onFilterChanged = (options: SpotlightOptions): void => {
-    setSelections(options);
-    setLocationsQuery(router, options, props.locations);
+    if (options.indicator) {
+      setSelections(options);
+      setLocationsQuery(router, options, props.locations);
+    }
   };
 
   return (
     <PageSection narrow>
       <Spotlight className="spotlight--full">
-        <LocationComparisonWrapper
-          themes={props.themes}
-          locations={props.locations}
-          countryCode={props.countryCode}
-          onFilterChanged={onFilterChanged}
-          options={selections}
-          onRemoveChart={props.onRemove}
-        />
+        <SpotlightBanner>
+          <SpotlightFilters
+            themes={props.themes}
+            onOptionsChange={onFilterChanged}
+            topicLabel="Select a topic to explore"
+            indicatorLabel="Choose an indicator"
+            topicClassName="form-field--inline-three"
+            indicatorClassName="form-field--inline-three"
+            yearClassName="hide"
+          >
+            <FormField className="form-field--inline-three">
+              <Button className="button button--alt button--icon-l" onClick={props.onRemove}>
+                <Icon name="-20 ico-cross-slate" />
+                Remove chart
+              </Button>
+            </FormField>
+          </SpotlightFilters>
+        </SpotlightBanner>
+        {selections.indicator ? (
+          <VisualisationSectionMain>
+            <LocationComparisonChartDataHandler
+              countryCode={props.countryCode}
+              locations={props.locations}
+              indicator={selections.indicator}
+            />
+          </VisualisationSectionMain>
+        ) : null}
       </Spotlight>
     </PageSection>
   );
