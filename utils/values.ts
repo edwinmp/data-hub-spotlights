b3c454b167dc27b283a71d8442dc015476c133de
Edwin P. Magezi <edwinm_p@yahoo.com>
2020-05-07 10:22:32 +0300

    Improve suffix handling for position

--- utils/values.ts
+++ utils/values.ts
@@ -13,5 +13,17 @@ export interface ValueOptions {
 export const DEFAULT_VALUE = 'No Data';
 
 export const addPrefixAndSuffix = (value: string | number, options: ValueOptions): string => {
+  if (options.suffix === 'th') {
+    if (value === 1 || value === '1') {
+      return `${options.prefix || ''} ${value}st`;
+    }
+    if (`${value}`.endsWith('2')) {
+      return `${options.prefix || ''} ${value}nd`;
+    }
+    if (`${value}`.endsWith('3')) {
+      return `${options.prefix || ''} ${value}rd`;
+    }
+  }
+
   return `${options.prefix || ''} ${value}${options.suffix || ''}`;
 };
